b6b4a6aa1aa3a632a58609f720d22dc8489c3cfa
Elvin Posadas <elvinjosuep2@gmail.com>
2023-08-20 18:17:25 -0600

    ejercicio1.js finished

---
+++ ejercicio1.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const student = {
+  firstName: "Elvin",
+  lastName: "Posadas Marquez",
+  courses: [
+    "Git & Github",
+    "Web 101",
+    "HTML y CSS",
+    "Intro a Javascript",
+    "Modelado de datos y SQL",
+    "Introduccion a Node.js",
+    "Desarrollo Front-End con JS",
+    "Front-end pro",
+    "Web components",
+    "Fundamentos de React",
+    "TDD con Javascript",
+    "Node.js Avanzado",
+    "React.js avanzado",
+    "Configuracion de serv",
+  ],
+  openToWork: false,
+  socialMedia: [
+    { name: "X", link: "https://www.twitter.com" },
+    { name: "Instagram", link: "https://www.instagram.com" },
+    { name: "Facebook", link: "https://www.facebook.com" },
+    {
+      name: "LinkedIn",
+      link: "https.//www.linkedind.com/in/elvin-posadas-18192978/",
+    },
+    { name: "GitHub", link: "https://www.github.com" },
+  ],
+};
